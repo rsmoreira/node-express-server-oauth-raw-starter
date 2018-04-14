7ecc29c04a2fead7ed691ea1579538ece7de4d73
rsmoreira <rsmoreira.mail@gmail.com>
2018-04-13 22:59:43 -0300

    Just testing the push to Github and Heroku again.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hey: 'buddy' });
+    res.send({ "it's me": "Mario!" });
 });
 
 const PORT = process.env.PORT || 5000;
