6eb82720f4584bcc92565af65761f8f907422992
rsmoreira <rsmoreira.mail@gmail.com>
2018-04-19 23:29:11 -0300

    Added Passport's Done Callback and added Passport's serializedUser function to generate and configure at the browser a Cookie.

--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,19 @@ const keys = require('../config/keys.js');
 // it gives access to Mongoose Model Class Users
 const User = mongoose.model('users');
 
+/**
+ * Configuring passport to treat the Cookie serialization and 
+ * deserialization.
+ * 
+ * The 'user' is the retrieved/authenticated user that we
+ * have work with on our GoogleStrategy callback function.
+ * 
+ */
+passport.serializeUser((user, done) => {
+    // This user.id, is the id generated on the Users Collection, on MongoDB.
+    // It isn't the profile.id.
+    done(null, user.id);
+});
 
 passport.use(
     new GoogleStrategy(
@@ -19,13 +32,28 @@ passport.use(
              * This is the callback function called after 
              * Google have validated with the user through 
              * OAuth.
+             * 
+             * DONE - Passport's callback
+             *  Take a look that, after retrieving the user 
+             *  from the datasource, or after creating it,
+             *  we are calling the function DONE. 
+             * 
+             *  Here, DONE is a Passport's Callback function
+             *  used to tell Passport that our callback is 
+             *  finished successfully. 
+             * 
+             *  Take a look that we are sending null as the first 
+             *  argument (the error first argument) and the created 
+             *  user or the retrieved used, if it is the case.
              */
             User.findOne({ googleId : profile.id })
                 .then((existingUser) => {
                     if (existingUser) {
-
+                        done(null, existingUser);
                     } else {
-                        new User({ googleId : profile.id }).save();
+                        new User({ googleId : profile.id })
+                            .save()
+                            .then(user => done(null, user));
                     }
                 });
             
