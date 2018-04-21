65041c275d7900a1fcb24ffc175e978f14e56578
rsmoreira <rsmoreira.mail@gmail.com>
2018-04-21 18:48:02 -0300

    Added logout feature.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 module.exports = (app) => {
 
     // Google OAuth Passport Strategy Routes
+    // Route to get authenticated with google
     app.get(
         '/auth/google', 
         passport.authenticate('google', {
@@ -15,6 +16,13 @@ module.exports = (app) => {
         '/auth/google/callback',
         passport.authenticate('google')
     );
+    
+    // Route to logout
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    })
+
 
     // Route to show current_use data
     app.get('/api/current_user', (req, res) => {
